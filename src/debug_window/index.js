5729a2da7d98ea0393d7f1d8dc8861a5f8d3b06d
an7759anan <an7759anan@rtural.ru>
2021-10-24 22:13:02 +0500

    main menu, waste...

--- src/debug_window/index.js
+++
@@ -1,3 +0,0 @@
-const i2c = require('i2c-bus');
-
-const TCA8418_ADDR = 0x34;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const controller = require ('./controller/controller');
+
 const path = require('path');
-let mainWindow, generatorWindow, debugWindow;
+let mainWindow;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -10,95 +12,25 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createMainWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
-};
-
-const createGeneratorWindow = () => {
-  // Create the browser window.
-  generatorWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
-  });
-
-  // and load the index.html of the app.
-  generatorWindow.loadFile(path.join(__dirname, '/generator/index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-const createDebugWindow = () => {
-  debugWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
+    alwaysOnTop: true,
+    fullscreen: true,
+    titleBarStyle: 'hidden',
     webPreferences: {
-	nodeIntegration: true,
-	contextIsolation: false,
-	preload: path.join(__dirname, '/debug_window/index.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, '/main_window/index.js'),
     }
   });
-  debugWindow.loadFile(path.join(__dirname, '/debug_window/index.html'));
+  mainWindow.loadFile(path.join(__dirname, '/main_window/index.html'));
+};
 
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-//app.on('ready', createWindow);
-
-const createWindows = ()  => {
-  createMainWindow();
-  createGeneratorWindow();
-  createDebugWindow();
-}
-
-let template = [{
-  label: 'Окно',
-  submenu: [{
-    label: 'Главное',
-    accelerator: 'F1',
-    role: 'main',
-    click: () => {
-      generatorWindow.hide();
-      mainWindow.show();
-    }
-  }, {
-    label: 'Генератор и Измеритель',
-    accelerator: 'F2',
-    role: 'generator',
-    click: () => {
-      mainWindow.hide();
-      generatorWindow.show();
-    }
-  }, {
-    label: 'Служебная связь',
-    accelerator: 'F3',
-    role: 'service-comm'
-  }, {
-    label: 'Debugg window',
-    accelerator: 'F4',
-    role: 'debugg',
-    click: () => {
-      mainWindow.hide();
-      debugWindow.show();
-    }
-  }]
-}];
+app.allowRendererProcessReuse=false;
 
 app.on('ready', () => {
-  createWindows();
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+  createMainWindow();
+  mainWindow.once('ready-to-show', () => {
+    controller.init(mainWindow);
+  })
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -114,10 +46,7 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
- //   createWindow();
-    createWindows();
+    createMainWindow();
+    controller.init();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
