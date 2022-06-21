965d202a9397fbf9224774b76e9851e4a1d967a8
an7759anan <an7759anan@rtural.ru>
2022-06-21 08:25:56 +0500

    dsp...

--- src/controller/dsp.js
+++ src/controller/dsp.js
@@ -7,13 +7,14 @@ const dsp_init = () => {
     return new Promise((resolve, reject) => {
         serialport = new SerialPort({ path: '/dev/ttyAMA0', baudRate: 115200, dataBits: 8, stopBits: 1, parity: 'none' });
         //        serialport.write('Hello!');
-       const rs = fs.createReadStream("./dsp.img");
-// const rs = fs.createReadStream("./Morning-Pictures.jpg");
-// const ws = fs.createWriteStream("./Morning-Pictures1.jpg");
-rs.on('error', (error) => {
+        const rs = fs.createReadStream("./dsp.img");
+        // const rs = fs.createReadStream("./Morning-Pictures.jpg");
+        // const ws = fs.createWriteStream("./Morning-Pictures1.jpg");
+        rs.on('error', (error) => {
             resolve(`error: ${error.message}`);
         });
         rs.on('data', (chunk) => {
+            console.log(`chunk: ${chunk.length}`);
             serialport.write(chunk);
         });
         // rs.on('data', (chunk) => {
@@ -28,4 +29,5 @@ rs.on('error', (error) => {
 
 module.exports = {
     "dsp_init": dsp_init,
+
 }
