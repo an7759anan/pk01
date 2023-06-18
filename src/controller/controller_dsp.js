125ef9c0ad4b9b57517858234bb6f55607527685
aaanikinperm <an7759anan@rtural.ru>
2023-06-18 15:28:25 +0500

    ...

--- src/controller/controller_dsp.js
+++ src/controller/controller_dsp.js
@@ -10,10 +10,10 @@ let vDm;
 let vCmd = null;
 let vStep = 0;
 
-// cmd["p2"] = vDm.settings["gen-tran-val"].val;   уровень выходного сигнала {min: -55, max: 3}, step: 1
-// cmd["p3.1"] = vDm.settings["gen-freq-val"].val; частота генератора
-// cmd["p6"] = vDm.settings["mes-voice1-val"].val; тип входа
-// cmd["p11"] = 5;                                шаг именения уровня
+// vCmd["p2"] = vDm.settings["gen-tran-val"].val;   уровень выходного сигнала {min: -55, max: 3}, step: 1
+// vCmd["p3.1"] = vDm.settings["gen-freq-val"].val; частота генератора
+// vCmd["p6"] = vDm.settings["mes-voice1-val"].val; тип входа
+// vCmd["p11"] = 5;                                шаг именения уровня
 
 const controller_dsp_init = (pDsp, pDm) => {
   vDsp = pDsp;
@@ -62,26 +62,26 @@ const sendStartCommand = (pScriptIdx) => {
       vCmd["p7"] = vDm.settings["mes-voice2-val"].val;
       break;
     case 2: // (2) Измерение отношения Сигнал/Шум
-      cmd["p2"] = dm.settings["gen-tran-val"].val;
-      cmd["p3.1"] = dm.settings["gen-freq-val"].val;
-      cmd["p6"] = dm.settings["mes-voice1-val"].val;
-      cmd["p11"] = 5;
+      vCmd["p2"] = dm.settings["gen-tran-val"].val;
+      vCmd["p3.1"] = dm.settings["gen-freq-val"].val;
+      vCmd["p6"] = dm.settings["mes-voice1-val"].val;
+      vCmd["p11"] = 5;
       break;
     case 3: // (3) Измерение шума свободного канала
-      // cmd["p2"] = vDm.settings["gen-tran-val"].val;
-      // cmd["p3.1"] = vDm.settings["gen-freq-val"].val;
-      // cmd["p6"] = vDm.settings["mes-voice1-val"].val;
-      // cmd["p11"] = 5;
+      // vCmd["p2"] = vDm.settings["gen-tran-val"].val;
+      // vCmd["p3.1"] = vDm.settings["gen-freq-val"].val;
+      // vCmd["p6"] = vDm.settings["mes-voice1-val"].val;
+      // vCmd["p11"] = 5;
       break;
     case 4: // (4) Измерение частотной характеристики
-      cmd["p2"] = dm.settings["gen-tran-val"].val;
-      cmd["p3.1"] = dm.settings["gen-freq-val"].val;
-      cmd["p3.2"] = 3600;
-      cmd["p12"] = 100;
+      vCmd["p2"] = dm.settings["gen-tran-val"].val;
+      vCmd["p3.1"] = dm.settings["gen-freq-val"].val;
+      vCmd["p3.2"] = 3600;
+      vCmd["p12"] = 100;
       break;
     case 5: // (5) Измерение амплитудной характеристики
-      cmd["p2"] = dm.settings["gen-tran-val"].val;
-      cmd["p11"] = 5;
+      vCmd["p2"] = dm.settings["gen-tran-val"].val;
+      vCmd["p11"] = 5;
       break;
     default:
       break;
