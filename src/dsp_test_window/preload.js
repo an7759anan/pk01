1b713b5ca48f2cb2c53e3eaf82d0f58601648077
aaanikinperm <an7759anan@rtural.ru>
2023-08-03 19:27:00 +0500

    windows divided ...

--- src/dsp_test_window/preload.js
+++ src/dsp_test_window/preload.js
@@ -1,11 +1,34 @@
-const { contextBridge, ipcRenderer } = require('electron');
-const { appendFile } = require('original-fs');
-const { KF } = require('../drivers/dsp');
+const { ipcRenderer } = require('electron');
+const { drawInit, drawData, drawPicture } = require('./drawings');
 
+ipcRenderer.on('CONTROLLER_TO_VIEW_MESSAGE', (evt, message) => {
+    if (message.screen == 'DSP_TEST_SCREEN') {
+        if (message.show == true) $('#dsp-test-screen').show();
+        if (message.value && message.value == 'DATA_TO_SERIALPORT'){
+            let $text = $('#logarea');
+            let dataFromDspText = `===> data: ${JSON.stringify(message.data)}`;
+            $text.val(`${dataFromDspText}\n${$text.val()}`)
+        }
+        if (message.value && message.value == 'DATA_FROM_SERIALPORT'){
+            let $text = $('#logarea');
+            let dataFromDspText = `<=== row: ${message.data.dataFromSerialPort}; data: ${JSON.stringify(message.data.dataFromDsp)}`;
+            if (message.data.dataFromDsp["p30"] === 2){
+                let d = message.data.dataFromDsp;
+                // dataFromDspText = `Вх.сиг: ${d["p8"]}; Шум: ${d["p9"]}; Ур.вх.сиг.: ${(20*Math.log10(d["p8"]/0.775)).toFixed(2)}; Отношение: ${(20*Math.log10(d["p8"]/d["p9"])).toFixed(2)}`
+                dataFromDspText = `Вх.сиг: ${d["p8"]}; Шум: ${d["p9"]}; Ур.вх.сиг.: ${(20*Math.log10(d["p8"]/10158)).toFixed(2)}; Отношение: ${(20*Math.log10(d["p8"]/d["p9"])).toFixed(2)}`
+            }
+            $text.val(`${dataFromDspText}\n${$text.val()}`)
+        }
+    }
+});
+
+/**
+ * DSP тестовое представление
+ */
 addEventListener('load', (event) => {
     ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'SERIAL_PORT_LIST'  })
     .then(result => {
-        $('#serial-port-select').append(result.map(el => `<option value="${el.path}">${el.friendlyName}</option>`).join(''));
+        $('#serial-port-select').append(result.map(el => `<option value="${el.path}">${el.friendlyName || el.path}</option>`).join(''));
     });
     $("#open-port").on('click', (e) => {
         ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'SERIAL_PORT_OPEN', path: $('#serial-port-select').val()})
@@ -46,16 +69,26 @@ addEventListener('load', (event) => {
                 default:
                     break;
             }
-                cmd["TEST"] = parseInt($tr.find('td[param="TEST"]').text());
-                cmd["PSOF"] = parseInt($tr.find('td[param="PSOF"]').text());
-                cmd["DB10"] = parseInt($tr.find('td[param="DB10"]').text());
-                break;
+            cmd["TEST"] = parseInt($tr.find('td[param="TEST"]').text());
+            cmd["PSOF"] = parseInt($tr.find('td[param="PSOF"]').text());
+            cmd["DB10"] = parseInt($tr.find('td[param="DB10"]').text());
+            ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'SEND_COMMAND_TO_DSP', cmd: cmd})
+            .then(result => {
+                let $text = $('#logarea');
+                $text.val(`===> ${result.success? result.content : 'error'}\n${$text.val()}`)
+            });
+            break;
+            case 42: // имитация ответа DSP
+            cmd["p30"] = parseInt($tr.find('td[param="30"]').text());
+            cmd["p4"] = parseInt($tr.find('td[param="4"]').text());
+            cmd["p3.1"] = parseInt($tr.find('td[param="3.1"]').text());
+            ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'SEND_COMMAND_FROM_DSP', cmd: cmd})
+            .then(result => {
+                let $text = $('#logarea');
+                $text.val(`===> ${result.success? result.content : 'error'}\n${$text.val()}`)
+            });
+            break;
         }
-        ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'SEND_COMMAND_TO_DSP', cmd: cmd})
-        .then(result => {
-            let $text = $('#logarea');
-            $text.val(`${$text.val()}\n===> ${result.success? result.content : 'error'}`)
-        });
     });
     $('#clear-log').on('click', () => {
         $('#logarea').val('');
@@ -64,24 +97,16 @@ addEventListener('load', (event) => {
         let $text = $('#logarea');
         ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'LOAD_DSP_SOFT' })
         .then(result => {
-            $text.val(`${$text.val()}\n<=== ${result}`)
+            $text.val(`<=== ${result}\n${$text.val()}`)
             console.log(result);
         })
         .catch(err => {
-            $text.val(`${$text.val()}\n<=== ${err}`)
+            $text.val(`<=== ${err}\n${$text.val()}`)
             console.log(result);
         })
-
     })
+    $('#touch-panel-screen button').on('click', e => {
+        let $button = $(e.target).closest('button');
+        ipcRenderer.invoke('VIEW_TO_CONTROLLER_MESSAGE', { command: 'EVENT_LOOP', key: +$button.attr('code') })
+    });
 });
-
-ipcRenderer.on('CONTROLLER_TO_VIEW_MESSAGE', (evt, message) => {
-    switch(message.type){
-        case 'DATA_FROM_SERIALPORT':
-            let $text = $('#logarea');
-            $text.val(`${$text.val()}\n<=== ${message.data}`)
-            
-        break;
-    }
-});
-
